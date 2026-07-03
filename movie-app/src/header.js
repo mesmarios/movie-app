6e3e7ccaf58205c380da9c1d15055fb95195ce8b
Romanos Papastamatiou <romanospapastamatiou@gmail.com>
2026-07-03 15:26:12 +0300

    first commit

--- movie-app/src/header.js
+++ movie-app/src/header.js
@@ -1,3 +1,11 @@
-export default function App() {
-  return <div>Hello</div>;
+function Clickable() {
+  return <button>⮟</button>;
 }
+export default function App() {
+  return <div><h1>Movie App</h1>
+  <Clickable />
+  transform: size(1.9)
+    </div>
+  ;
+}
+
